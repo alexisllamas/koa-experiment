6665f30a05a69ad88984711edeed31a8a43fbc2f
Alexis Navarro <alexisnavarro23@gmail.com>
2018-05-29 22:46:38 +0000

    Add babel-eslint

--- webapp/index.js
+++ webapp/index.js
@@ -4,22 +4,21 @@ import './styles/index.scss'
 
 class MyComponent extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      counter: 0
-    };
+      counter: 0,
+    }
   }
   render() {
     return (
       <div>
         <h1>Header!</h1>
-        <span $HasVNodeChildren>{createTextVNode('Counter is at: ' + this.state.counter)}</span>
+        <span $HasVNodeChildren>
+          {createTextVNode('Counter is at: ' + this.state.counter)}
+        </span>
       </div>
-    );
+    )
   }
 }
 
-render(
-  <MyComponent />,
-  document.getElementById("app")
-)
+render(<MyComponent />, document.getElementById('app'))
